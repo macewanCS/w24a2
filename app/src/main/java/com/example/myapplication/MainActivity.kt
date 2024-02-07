d43e5e977f7e392402a886aba4e84ca5f7f24d28
Angelica B. Student <anbilliones@gmail.com>
2024-02-07 15:25:40 -0700

    Edited home fragment by hardcoding a list of Upcoming Events

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -24,6 +24,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //CheckedTextView() checkedTextView = findViewById(R.id.Session1)
     }
 }
