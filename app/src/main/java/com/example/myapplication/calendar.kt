15e69405928dff36b886a3f4999dd9c86ab866a7
JheaneyPerico <jheaneyperico@gmail.com>
2024-02-13 12:45:20 -0700

    Added functionality code to calendar.kt, making it functional with the bottom navigation bar. Refractored and renamed calendar layout xml. changed var names

--- app/src/main/java/com/example/myapplication/calendar.kt
+++ app/src/main/java/com/example/myapplication/calendar.kt
@@ -16,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [homePage.newInstance] factory method to
+ * Use the [calendar.newInstance] factory method to
  * create an instance of this fragment.
  */
 class calendar : Fragment() {
