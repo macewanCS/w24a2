1e236e8cbd8f18a1ee96435115668c962a108693
JheaneyPerico <jheaneyperico@gmail.com>
2024-02-13 12:43:31 -0700

    Added functionality code to calendar.kt, making it functional with the bottom navigation bar. Refractored and renamed calendar layout xml.

--- app/src/main/java/com/example/myapplication/calendar.kt
+++ app/src/main/java/com/example/myapplication/calendar.kt
@@ -89,7 +89,7 @@ class calendar : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            homePage().apply {
+            calendar().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
