6a84f66e060a24858dffd93fb20f62a97408656b
Maren R. Student <rogersm26@mymacewan.ca>
2024-02-29 17:14:00 -0700

    Logout button added to account page

---
+++ app/src/main/java/com/example/myapplication/Logout.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+
+class Logout : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_account, container, false)
+
+        val logoutBtn: Button = view.findViewById(R.id.logout)
+
+        logoutBtn.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+
+            findNavController().navigate(R.id.to_login)
+        }
+        return view
+    }
+}
