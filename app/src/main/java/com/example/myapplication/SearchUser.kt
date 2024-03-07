969f8b2047e2a2aec42b3beaf9f56100f876ba73
JheaneyPerico <jheaneyperico@gmail.com>
2024-03-06 23:10:28 -0700

    Initialized variable buttons, design, and search inputs on SearchUser class

---
+++ app/src/main/java/com/example/myapplication/SearchUser.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+
+class SearchUser : AppCompatActivity() {
+    private var searchInput: EditText? = null
+    private var searchButton: ImageButton? = null
+    private var backButton: ImageButton? = null
+    private var recyclerView: RecyclerView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_search_user)
+
+        // initialize layout buttons and design
+        searchInput = findViewById(R.id.search_username_input)
+        searchButton = findViewById(R.id.search_user_btn)
+        backButton = findViewById(R.id.back_btn)
+        recyclerView = findViewById(R.id.search_user_recycler_view)
+    }
+}
